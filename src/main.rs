4f55f4474a6d32926d6265c428b214f9d8d36509
container1 <hans@d-jschlich.dvspla.de>
2024-11-25 14:04:49 +0100

    env variable support

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,17 @@ use bsky_sdk::BskyAgent;
 use bsky_sdk::api;
 use api::types::string::Datetime;
 use std::io;
-
+use std::env;
+use dotenv::dotenv;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    dotenv().ok();
+    
+    let pw = env::var("BSKY_PW").unwrap();            
+    let mail = env::var("BSKY_MAIL").unwrap();
     let agent = BskyAgent::builder().build().await?;
-    let _session = agent.login("mail", "apppasswd").await?;
+    let _session = agent.login(mail, pw).await?;
     let mut input = String::new();
     io::stdin().read_line(&mut input).expect("Failed to read line");
     agent.create_record(api::app::bsky::feed::post::RecordData {
